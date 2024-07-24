80fb0297dc32880d93638c46f67912467f9074cd
mkchun <yikl1004@gmail.com>
2024-07-24 16:34:42 +0900

    prettier-config 업데이트

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    quoteProps: 'as-needed',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: true,
+};
